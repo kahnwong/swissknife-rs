6ea66f66aed5ff9d4584b4ebed6211cde13f8977
Karn Wong <karn@karnwong.me>
2025-04-29 10:39:48 +0700

    extract to config.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+mod config;
 use clap::{Parser, Subcommand};
+use config::CLAP_STYLING;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None, styles = CLAP_STYLING)]
@@ -46,12 +48,3 @@ fn main() {
         }
     }
 }
-// See also `clap_cargo::style::CLAP_STYLING`
-pub const CLAP_STYLING: clap::builder::styling::Styles = clap::builder::styling::Styles::styled()
-    .header(clap_cargo::style::HEADER)
-    .usage(clap_cargo::style::USAGE)
-    .literal(clap_cargo::style::LITERAL)
-    .placeholder(clap_cargo::style::PLACEHOLDER)
-    .error(clap_cargo::style::ERROR)
-    .valid(clap_cargo::style::VALID)
-    .invalid(clap_cargo::style::INVALID);
